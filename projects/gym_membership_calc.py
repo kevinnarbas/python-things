829c5d919a01f31b98948fa4cfe3ffd82b9ff3de
kevinnarbas <kevinnarbas@git.generalassemb.ly>
2019-09-05 12:38:45 -0500

    adds annual fee addition

--- projects/gym_membership_calc.py
+++ projects/gym_membership_calc.py
@@ -1,6 +1,7 @@
 initial_cost = int(input('Enter the initiation fee: $'))
 monthly_cost = int(input('Enter the monthly fee: $'))
 length = int(input('Enter how many months you\'d like to calculate for: '))
+annual_cost = int(input('Enter the annual cost: $'))
 
 total_cost = []
 
@@ -9,9 +10,12 @@ for num in range(length + 1):
     total_cost.append(initial_cost)
   elif num == 1:
     total_cost.append(monthly_cost + initial_cost)
+  elif num % 12 == 0:
+    total_cost.append(monthly_cost + total_cost[-1] + annual_cost)
   else:
     total_cost.append(total_cost[-1] + monthly_cost)
 
+print(total_cost)
 
 print(f'With an initial cost of ${initial_cost} and a monthly cost of ${monthly_cost} after {length} months your total will be ${total_cost[-1]} with a first month\'s payment of ${total_cost[1]}')
   
